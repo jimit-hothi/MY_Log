3f469b9fc64de0ef40ac70b0a7422a99ce9cd720
jimit-ikraft <jimit@ikraftsolutions.com>
2017-03-21 13:35:53 +0530

    MY_Log.php File

---
+++ MY_Log.php
@@ -0,0 +1,84 @@
+<?php
+
+class MY_Log extends CI_Log {
+
+    public function write_log($level, $msg)
+    {
+        if ($this->_enabled === FALSE)
+        {
+            return FALSE;
+        }
+
+        $level = strtoupper($level);
+
+        if (( ! isset($this->_levels[$level]) OR ($this->_levels[$level] > $this->_threshold))
+            && ! isset($this->_threshold_array[$this->_levels[$level]]))
+        {
+            return FALSE;
+        }
+
+        $filepath = $this->_log_path . $level .'-'. date('d-m-Y').'.'.$this->_file_ext;
+
+        $message = '';
+
+        if ( ! file_exists($filepath))
+        {
+            $newfile = TRUE;
+            // Only add protection to php files
+            if ($this->_file_ext === 'php')
+            {
+                $message .= "";
+            }
+        }
+
+        if ( ! $fp = @fopen($filepath, 'ab'))
+        {
+            return FALSE;
+        }
+
+        flock($fp, LOCK_EX);
+
+        // Instantiating DateTime with microseconds appended to initial date is needed for proper support of this format
+        if (strpos($this->_date_fmt, 'u') !== FALSE)
+        {
+            $microtime_full = microtime(TRUE);
+            $microtime_short = sprintf("%06d", ($microtime_full - floor($microtime_full)) * 1000000);
+            $date = new DateTime(date('d-m-Y H:i:s.'.$microtime_short, $microtime_full));
+            $date = $date->format($this->_date_fmt);
+        }
+        else
+        {
+            $date = date($this->_date_fmt);
+        }
+
+        $message .= $this->_format_line($level, $date, $msg);
+
+        for ($written = 0, $length = strlen($message); $written < $length; $written += $result)
+        {
+            if (($result = fwrite($fp, substr($message, $written))) === FALSE)
+            {
+                break;
+            }
+        }
+
+        flock($fp, LOCK_UN);
+        fclose($fp);
+        
+        $unlink_date = date('Y-m-d',strtotime("-1 days"));
+        
+        $filepath_unlink = $this->_log_path . $level .'-'. $unlink_date.'.'.$this->_file_ext;
+        
+        if ( file_exists($filepath_unlink))
+        {
+        	unlink($filepath_unlink);
+        }
+        
+
+        if (isset($newfile) && $newfile === TRUE)
+        {
+            chmod($filepath, $this->_file_permissions);
+        }
+
+        return is_int($result);
+    }
+}
